1bf33398b1b7d1206594c5678593f79a41245d6a
Victor Nascimento FAI <victor.nascimento@findai.com.br>
2025-04-01 14:40:17 -0300

    Update data loading and filtering in Streamlit app for hospital establishments

--- ops_prestadores_streamlit.py
+++ ops_prestadores_streamlit.py
@@ -8,30 +8,41 @@ from streamlit_folium import st_folium
 @st.cache_data
 def carregar_dados():
     # Substitua pelo caminho absoluto do arquivo CSV
-    return pd.read_csv('tb_estab_sp_hosp.csv', encoding='latin1', sep=';')
+    return pd.read_csv('tb_estab_sp_filtrado.csv', encoding='latin1', sep=';')
 
-tb_estab_sp_hosp = carregar_dados()
+tb_estab_sp_filtrado = carregar_dados()
 
 # Converter colunas de latitude e longitude para float
-tb_estab_sp_hosp['NU_LATITUDE'] = tb_estab_sp_hosp['NU_LATITUDE'].astype(float)
-tb_estab_sp_hosp['NU_LONGITUDE'] = tb_estab_sp_hosp['NU_LONGITUDE'].astype(float)
+tb_estab_sp_filtrado['NU_LATITUDE'] = tb_estab_sp_filtrado['NU_LATITUDE'].astype(float)
+tb_estab_sp_filtrado['NU_LONGITUDE'] = tb_estab_sp_filtrado['NU_LONGITUDE'].astype(float)
 
 # Título do aplicativo
 st.title("Mapa de Estabelecimentos Hospitalares")
 
+# Adicionar filtro por tipo de estabelecimento (sem a opção "Todos")
+tipo_selecionado = st.selectbox(
+    "Selecione o tipo de estabelecimento:",
+    options=tb_estab_sp_hosp['DS_TIPO_ESTABELECIMENTO'].unique().tolist(),
+    index=2
+)
+
+# Filtrar os dados com base no tipo selecionado
+tb_estab_sp_filtrado = tb_estab_sp_filtrado[tb_estab_sp_filtrado['DS_TIPO_ESTABELECIMENTO'] == tipo_selecionado]
+
 # Adicionar filtro por zona
 zona_selecionada = st.selectbox(
     "Selecione uma zona da cidade:",
-    options=["Todas"] + tb_estab_sp_hosp['ZONA_SP'].unique().tolist()
+    options=["Todas"] + tb_estab_sp_filtrado['ZONA_SP'].unique().tolist(),
+    index=2
 )
 
 # Filtrar os dados com base na zona selecionada
 if zona_selecionada != "Todas":
-    tb_estab_sp_hosp = tb_estab_sp_hosp[tb_estab_sp_hosp['ZONA_SP'] == zona_selecionada]
+    tb_estab_sp_filtrado = tb_estab_sp_filtrado[tb_estab_sp_filtrado['ZONA_SP'] == zona_selecionada]
 
 # Criar um mapa centralizado na média das coordenadas
-latitude_media = tb_estab_sp_hosp['NU_LATITUDE'].mean()
-longitude_media = tb_estab_sp_hosp['NU_LONGITUDE'].mean()
+latitude_media = tb_estab_sp_filtrado['NU_LATITUDE'].mean()
+longitude_media = tb_estab_sp_filtrado['NU_LONGITUDE'].mean()
 
 mapa = folium.Map(location=[latitude_media, longitude_media], zoom_start=10)
 
@@ -39,7 +50,7 @@ mapa = folium.Map(location=[latitude_media, longitude_media], zoom_start=10)
 marker_cluster = MarkerCluster().add_to(mapa)
 
 # Adicionar os pontos ao mapa
-for _, row in tb_estab_sp_hosp.iterrows():
+for _, row in tb_estab_sp_filtrado.iterrows():
     folium.Marker(
         location=[row['NU_LATITUDE'], row['NU_LONGITUDE']],
         popup=row.get('NO_FANTASIA', 'Sem Nome')  # Substitua pela coluna desejada
@@ -50,7 +61,7 @@ st_data = st_folium(mapa, width=700, height=500)
 
 # Selecionar as colunas desejadas para exibição na tabela
 colunas_tabela = ['CO_CNES', 'NO_FANTASIA', 'NO_LOGRADOURO', 'NU_ENDERECO', 'NO_COMPLEMENTO', 'CO_CEP', 'ZONA_SP', 'NU_TELEFONE', 'NO_EMAIL']
-tabela_dados = tb_estab_sp_hosp[colunas_tabela]
+tabela_dados = tb_estab_sp_filtrado[colunas_tabela]
 
 # Exibir a tabela abaixo do mapa
 st.subheader("Detalhes dos Estabelecimentos")
